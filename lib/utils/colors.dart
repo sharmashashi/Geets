1f288dba6c36225a6c593d8740f3d97d010f3d5c
Shashi <shashi@kali.localhost>
2020-07-12 22:02:54 +0545

    added color classes

---
+++ lib/utils/colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+class LightColors{
+  static Color get shadowColor=>Color(0x00accfef);
+  static Color get background=>Color(0x00ccdefa);
+  static Color get purple1=>Color(0x009a97b4);
+  static Color get purple=>Color(0x0094589b);
+  static Color get purpleDark=>Color(0x00785aa0);
+  static Color get dark=>Color(0x00352351);
+  static Color get purple2=>Color(0x00ad77a3);
+
+}
+class DarkColors{
+  static Color get dark=>Color(0x00050606);
+  static Color get background=>Color(0x0047494b);
+  static Color get red1 =>Color(0x00d11302);
+  static Color get red2=>Color(0x00a30d02);
+  static Color get redDark=>Color(0x005d0f04);
+  static Color get brown=>Color(0x00ca5421);
+  static Color get lightPink=>Color(0x00f5a691);
+
+}
